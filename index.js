dc694e7811c054fc80a6e79cb3b3f844dcc175c4
mike <mikesapienzaaudio@gmail.com>
2020-11-02 10:31:00 -0600

    finished

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, match){
+    return drivers.filter(driver => driver.toLowerCase() === match.toLowerCase())
+}
+
+function fuzzyMatch(drivers, match){
+    return drivers.filter(driver => driver.toLowerCase().indexOf(match.toLowerCase()) === 0)
+}
+
+function matchName(drivers, match){
+    return drivers.filter(driver => driver.name === match)
+}
